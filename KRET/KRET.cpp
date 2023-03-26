217a360ddc80c3a8091e781cfdeb10106b45f858
Pasha Kirillov <89199389177lol@gmail.com>
2023-03-26 20:25:55 +0300

    clear, cls

--- KRET/KRET.cpp
+++ KRET/KRET.cpp
@@ -290,7 +290,7 @@ int main()
 	setlocale(LC_ALL, "Russian");
 
 	string str = "";
-	int number = NULL;
+	int number = 0;
 	NumberAsString X;
 
 	while (true)
@@ -315,19 +315,21 @@ int main()
 		{	}
 		catch (...){}
 
-		if (number != NULL)
+		if (number != 0)
 		{
 			X.Set(str);
-			number = NULL;
+			number = 0;
 			X.PrintString();
 
 			cout << "Выход - Q, ввести число заново - R" << endl;
 			cin >> str;
+			system("clear");
 			system("cls");
 		}
 
 		if (str == "R" || str == "r")
 		{
+			system("clear");
 			system("cls");
 		}
 		if (str == "Q"  || str == "q")
